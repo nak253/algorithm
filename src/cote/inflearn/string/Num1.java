bfe3b1607b1e47baf28ff6eb47e4cd8980ddafe0
nak253 <nwon4413@naver.com>
2022-01-05 16:56:15 +0900

    인프런 코딩테스트 String 1번 완료

---
+++ src/cote/inflearn/string/Num1.java
@@ -0,0 +1,22 @@
+package cote.inflearn.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().toLowerCase();
+        char c = br.readLine().toLowerCase().charAt(0);
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
