ecb8d4d41c3ac7936b4f26b8dc17375735634d2e
nak253 <nwon4413@naver.com>
2022-01-09 23:53:08 +0900

    인프런 코딩테스트 String 5번 완료

---
+++ src/cote/inflearn/string/Num5.java
@@ -0,0 +1,46 @@
+package cote.inflearn.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Num5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+
+    private static String solution(String str) {
+        char[] cArr = str.toCharArray();
+        int count = cArr.length-1;
+
+        for (int i = 0; i <=count; i++) {
+            if (isAlphabet(cArr[i])){
+                for (int j = count; i < j; j--) {
+                    if (isAlphabet(cArr[j])){
+                        char tmp = cArr[i];
+                        cArr[i] = cArr[j];
+                        cArr[j] = tmp;
+                    }
+                }
+            }
+
+        }
+
+        return new String(cArr);
+    }
+
+    private static boolean isAlphabet(char c) {
+        if ('a' <= c && c <= 'z') {
+            return true;
+        }
+        if ('A' <= c && c <= 'Z') {
+            return true;
+        }
+        return false;
+    }
+}
