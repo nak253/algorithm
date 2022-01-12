43ff1a2c1f022b76178fb7bd67d8dd7744ad0fb1
nak253 <nwon4413@naver.com>
2022-01-12 17:09:19 +0900

    LeetCode Reverse Integer 수정 : 인수를 String으로 변환하는 과정 수식으로 변경, result가 int타입의 범위를 벗어나는 경우 체크를 Integer.MIN_VALUE와 MAX_VALUE로 변경

--- src/leetCode/ReverseInteger.java
+++ src/leetCode/ReverseInteger.java
@@ -1,43 +1,20 @@
 package leetCode;
 
-import java.util.Arrays;
-
 public class ReverseInteger {
     public static int reverse(int x) {
+        long result = 0;
 
-        String numStr = String.valueOf(x);
-        boolean isMinus = false;
-
-        if (numStr.contains("-")) {
-            isMinus = true;
-            numStr = numStr.substring(1);
-        }
-        char[] numCharArr = numStr.toCharArray();
-        int lastIndex = numCharArr.length - 1;
-        for (int i = 0; i < numCharArr.length / 2; i++) {
-            char tmp = numCharArr[i];
-            numCharArr[i] = numCharArr[lastIndex];
-            numCharArr[lastIndex] = tmp;
-            lastIndex--;
-
+        while (x != 0) {
+            int onesPlace = x % 10;
+            x = x / 10;
+            result = (result * 10) + onesPlace;
         }
 
-        long l = Long.parseLong(String.valueOf(numCharArr));
-        if (!(Math.pow(-2, 31) <= l && l <= Math.pow(2, 31) - 1)) {
+        if (!(Integer.MIN_VALUE <= result && result <= Integer.MAX_VALUE)) {
             return 0;
         }
-        int result = (int) l;
-        if (isMinus) {
-            return result * -1;
-        }
-        return result;
-    }
+        int intResult = (int) result;
 
-    public static void main(String[] args) {
-        System.out.println(reverse(321));
-        System.out.println(reverse(-123));
-        System.out.println(reverse(120));
-        System.out.println(reverse(10));
-        System.out.println(reverse(1534236469));
+        return intResult;
     }
 }
