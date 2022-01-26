bb4fb01f5354f26bb907546b40526836cd55463b
nak253 <nwon4413@naver.com>
2022-01-26 15:25:04 +0900

    HackerRank SaveThePrisoner 완료

--- src/hackerrank/SaveThePrisoner.java
+++ src/hackerrank/SaveThePrisoner.java
@@ -3,14 +3,11 @@ package hackerrank;
 public class SaveThePrisoner {
 
     public static int saveThePrisoner(int n, int m, int s) {
-        if (m == 1) {
-            return s;
-        }
+        int num = (m + (s - 1)) % n;
 
-        int answer = (m%n) + (s-1);
-        if ( answer % n == 0) {
+        if (num == 0) {
             return n;
         }
-        return answer%n;
+        return num;
     }
 }
