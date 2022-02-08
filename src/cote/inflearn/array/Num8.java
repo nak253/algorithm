70c5ec4450fbfc364af490986f17ff898042db36
nak253 <nwon4413@naver.com>
2022-02-08 16:30:54 +0900

    인프런 코딩테스트 Array 8번 수정

--- src/cote/inflearn/array/Num8.java
+++ src/cote/inflearn/array/Num8.java
@@ -1,27 +1,31 @@
 package cote.inflearn.array;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Num8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        Integer[] intArr = new Integer[size];
+        int[] intArr = new int[size];
+        int[] answer = new int[size];
+
         for (int i = 0; i < intArr.length; i++) {
             intArr[i] = scanner.nextInt();
         }
-        Integer[] sortArr = Arrays.copyOf(intArr, size);
-        Arrays.sort(sortArr, Collections.reverseOrder());
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size ; i++) {
+            int rank = 1;
             for (int j = 0; j < size; j++) {
-                if (intArr[i] == sortArr[j]) {
-                    System.out.print(j + 1 + " ");
-                    break;
+                if (intArr[i] < intArr[j]) {
+                    rank++;
                 }
             }
+            answer[i] = rank;
         }
+
+        for (int result:answer) {
+            System.out.print(result + " ");
+        }
+
     }
 }
