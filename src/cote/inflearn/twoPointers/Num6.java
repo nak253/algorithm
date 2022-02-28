4e369ef07cdfaacf14e4a00f48b1b8c757fbbd25
nak253 <nwon4413@naver.com>
2022-03-01 01:13:09 +0900

    인프런 코딩테스트 Two pointers 6번 완료

---
+++ src/cote/inflearn/twoPointers/Num6.java
@@ -0,0 +1,22 @@
+package cote.inflearn.twoPointers;
+
+import java.util.Scanner;
+
+public class Num6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int numberOfChanges = scanner.nextInt();
+        int[] intArr = new int[size];
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        int count = 0;
+        int leftPointer = 0;
+        for (int rightPointer = 0; rightPointer < intArr.length; rightPointer++) {
+
+        }
+    }
+}
