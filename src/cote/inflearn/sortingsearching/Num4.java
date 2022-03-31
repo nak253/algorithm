b2e8507b95db4a9026a86df27bd2d18b79de4234
nak253 <nwon4413@naver.com>
2022-03-31 15:15:00 +0900

    인프런 코딩테스트 Sorting and Searching 4번 완료

--- src/cote/inflearn/sortingsearching/Num4.java
+++ src/cote/inflearn/sortingsearching/Num4.java
@@ -1,21 +1,39 @@
 package cote.inflearn.sortingsearching;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Num4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        int num = scanner.nextInt();
-        int[] arr = new int[size];
-        for (int i = 0; i < size; i++) {
+        int s = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-        for (int number: solution(size,num,arr)) {
-            System.out.println(number + " ");
+        for (int number : solution(s, n, arr)) {
+            System.out.print(number + " ");
         }
     }
 
-    private static int[] solution(int size, int num, int[] arr) {
+    private static int[] solution(int s, int n, int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[s];
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j < arr.length ; j++) {
+                if (arr[i] == arr[j]) {
+                    break;
+                }
+                if (j == arr.length-1) {
+                    stack.push(arr[i]);
+                }
+            }
+        }
+        stack.push(arr[n-1]);
+        for (int i = 0; i < s; i++) {
+            result[i] = stack.pop();
+        }
+        return result;
     }
 }
